ed21ee517cc7d5f72343f6744769b9df029c7ac1
Julian Peeters <julianpeeters@gmail.com>
2013-05-09 01:00:21 -0700

    1st success with asm-salat

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -3,37 +3,21 @@ import com.novus.salat._
 import com.novus.salat.global._
 import com.mongodb.casbah.Imports._
 
-
-  //class MyRec(myRec:MyRecord) extends AnyRef {}
-  case class MyRec extends AnyRef 
 object Main extends App {
-  type MyRecord = AnyRef{val x: String} 
-  //type MyRecord = {MyRec.asInstanceOf[MyRecord]}
-  
 
-  val model$ = DynamicClassLoader.loadClass("models.MyRecord$", MyRecord$Dump.dump()) //Load the module class
-  val model  = DynamicClassLoader.loadClass("models.MyRecord", MyRecordDump.dump()) //Then load the class
+  val model  = DynamicClassLoader.loadClass("models.MyRecord", MyRecordDump.dump()) // load the class
+  val model$ = DynamicClassLoader.loadClass("models.MyRecord$", MyRecord$Dump.dump()) //load the module class
+
   val ctor$  = model$.getConstructor()
-  val instance$ = ctor$.newInstance()//Change Module$Dump <init> from PRIVATE to PUBLIC, else no method found
+  val instance$ = ctor$.newInstance()//Changed Module$Dump <init> from PRIVATE to PUBLIC, else no method found
 
-  //val instance$ = model$.newInstance()
-  val apply$ = model$.getMethod("apply", classOf[String])
-  val record =  apply$.invoke(instance$, "hello world").asInstanceOf[CaseClass with Product]
-  //val record =  apply$.invoke(instance$, "hello world").asInstanceOf[MyRecord]
+  val apply$ = model$.getMethod("apply", classOf[String])//populate the instance with values,
+  val record =  apply$.invoke(instance$, "hello world")//roughly equivalent to `MyRecord("hello world"`)
 
-
- // println(returnType)
-//type p <: scala.Product
-  //val hello = MyRecord("helloworld")
-
-  val dbo = grater[MyRecord].asDBObject(record)
-
- // def dbo[MyRecord](rec: MyRecord) = grater[MyRecord].asDBObject(record)
- // def dbo[MyRecord](rec: MyRecord) = grater[AnyRef with Product].asDBObject(record.asInstanceOf[AnyRef with Product])
-   //val dbo = grater[MyRecord].asDBObject(record)
- 
- println(dbo(record))
-  //println()
+  val dbo = grater[record.type].asDBObject(record)
+    println(dbo)
+  val obj = grater[record.type].asObject(dbo)
+    println(obj)
 
 }
 
---
+++ src/main/scala/MyRecord.scala.bak
@@ -0,0 +1,3 @@
+package models
+
+case class MyRecord(x: String)
