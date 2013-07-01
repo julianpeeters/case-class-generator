d419a83e3ccf6091cde6cebad5a3bfc84262f031
Julian Peeters <julianpeeters@gmail.com>
2013-07-01 15:46:57 -0700

    trying to decide what to name the template-record maker

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -5,6 +5,11 @@ import com.mongodb.casbah.Imports._
 
 object Main extends App {
 
+  def createRecord(fieldVal: String) = {
+    val model  = DynamicClassLoader.loadClass("models.MyRecord", MyRecordDump.dump().get(0)) // load the class
+    val model$ = DynamicClassLoader.loadClass("models.MyRecord$", MyRecordDump.dump().get(1)) //load the module class
+    applyValues(model$, fieldVal) //get an instance
+  }
 
   def applyValues(classModule: java.lang.Class[_], fieldValue: String) = {
     val method_apply = classModule.getMethod("apply", classOf[String])//populate the instance with values,
@@ -12,13 +17,8 @@ object Main extends App {
     method_apply.invoke(classInstance, fieldValue)//roughly equivalent to using the statement `MyRecord(fieldValue)`
   }
 
-  def MyRecord(fieldVal: String) = {
-    val model  = DynamicClassLoader.loadClass("models.MyRecord", MyRecordDump.dump().get(0)) // load the class
-    val model$ = DynamicClassLoader.loadClass("models.MyRecord$", MyRecordDump.dump().get(1)) //load the module class
-    applyValues(model$, fieldVal) //get an instance
-  }
 
-  val record = MyRecord("hello world")
+  val record = createRecord("hello world") //Rename def to MyRecord to preserve the look and feel of salat??
   type MyRecord = record.type
   
   val dbo = grater[MyRecord].asDBObject(record)
