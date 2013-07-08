c80030cc3880873695f2db1a8aa23f1c300f05c6
Julian Peeters <julianpeeters@gmail.com>
2013-07-07 18:53:26 -0700

    working while loading from a dump list

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -5,31 +5,29 @@ import com.mongodb.casbah.Imports._
 
 object Main extends App {
 
-  def createRecord(fieldVal: String) = {
+  object MyRecord { 
     val model  = DynamicClassLoader.loadClass("models.MyRecord", MyRecordDump.dump().get(0)) // load the class
     val model$ = DynamicClassLoader.loadClass("models.MyRecord$", MyRecordDump.dump().get(1)) //load the module class
-    applyValues(model$, fieldVal) //get an instance
+
+    def apply(fieldValue: String) = {
+      val method_apply = model$.getMethod("apply", classOf[String])//populate the instance with values,
+      val classInstance = model$.getConstructor().newInstance()//getInstance(module)
+      method_apply.invoke(classInstance, fieldValue)//roughly equivalent to using the statement `MyRecord(fieldValue)`
+    }
+    val typeTemplate = MyRecord("hello world") //Rename def to MyRecord to preserve the look and feel of salat??
+    type MyRecord = typeTemplate.type
   }
 
-  def applyValues(classModule: java.lang.Class[_], fieldValue: String) = {
-    val method_apply = classModule.getMethod("apply", classOf[String])//populate the instance with values,
-    val classInstance = classModule.getConstructor().newInstance()//getInstance(module)
-    method_apply.invoke(classInstance, fieldValue)//roughly equivalent to using the statement `MyRecord(fieldValue)`
-  }
-
-
-  val record = createRecord("hello world") //Rename def to MyRecord to preserve the look and feel of salat??
-  type MyRecord = record.type
   
-  val dbo = grater[MyRecord].asDBObject(record)
+  val dbo = grater[MyRecord.MyRecord].asDBObject(MyRecord.typeTemplate)
 
     println(dbo)
 
-  val obj = grater[MyRecord].asObject(dbo)
+  val obj = grater[MyRecord.MyRecord].asObject(dbo)
     println(obj)
  
-  println(record == obj)
- 
+//  println(typeTemplate == obj)
+   println(MyRecord.typeTemplate == obj)
 }
 
 
