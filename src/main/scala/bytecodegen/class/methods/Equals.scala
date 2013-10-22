d4eba1164532600d33385a86e030ed0b095cb4a6
Julian Peeters <julianpeeters@gmail.com>
2013-10-22 00:54:03 -0700

    cleaned up equals method

--- src/main/scala/bytecodegen/class/methods/Equals.scala
+++ src/main/scala/bytecodegen/class/methods/Equals.scala
@@ -6,159 +6,29 @@ import Opcodes._
 
 case class Equals(cw: ClassWriter, var mv: MethodVisitor, caseClassName: String, fieldData: List[FieldData]) {
   def dump = {
-/*
-mv = cw.visitMethod(ACC_PUBLIC, "equals", "(Ljava/lang/Object;)Z", null, null);
-mv.visitCode();//if there's a "Nothing" then drop all value members after the first "Nothing".
-val fields = (if (fieldData.map(n=>n.fieldType).contains("Nothing")) fieldData.reverse.dropWhile(valueMember =>  valueMember.fieldType != "Nothing").reverse; else fieldData)
-var l0: Label = null
 
-mv = cw.visitMethod(ACC_PUBLIC, "equals", "(Ljava/lang/Object;)Z", null, null);
-mv.visitCode();
-mv.visitVarInsn(ALOAD, 0);
-mv.visitVarInsn(ALOAD, 1);
-l0 = new Label();
-mv.visitJumpInsn(IF_ACMPEQ, l0);
-mv.visitVarInsn(ALOAD, 1);
-mv.visitVarInsn(ASTORE, 2);
-mv.visitVarInsn(ALOAD, 2);
-mv.visitTypeInsn(INSTANCEOF, "models/MyRecord");
-val l1 = new Label();
-mv.visitJumpInsn(IFEQ, l1);
-mv.visitInsn(ICONST_1);
-mv.visitVarInsn(ISTORE, 3);
-val l2 = new Label();
-mv.visitJumpInsn(GOTO, l2);
-mv.visitLabel(l1);
-mv.visitFrame(Opcodes.F_APPEND,1, Array[Object] ("java/lang/Object"), 0, null);
-mv.visitInsn(ICONST_0);
-mv.visitVarInsn(ISTORE, 3);
-mv.visitLabel(l2);
-mv.visitFrame(Opcodes.F_APPEND,1, Array[Object] (Opcodes.INTEGER), 0, null);
-mv.visitVarInsn(ILOAD, 3);
-val l3 = new Label();
-mv.visitJumpInsn(IFEQ, l3);
-mv.visitVarInsn(ALOAD, 1);
-mv.visitTypeInsn(CHECKCAST, "models/MyRecord");
-mv.visitVarInsn(ASTORE, 4);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "a", "()B");
-mv.visitVarInsn(ALOAD, 4);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "a", "()B");
-val l4 = new Label();
-mv.visitJumpInsn(IF_ICMPNE, l4);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "b", "()S");
-mv.visitVarInsn(ALOAD, 4);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "b", "()S");
-mv.visitJumpInsn(IF_ICMPNE, l4);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "c", "()I");
-mv.visitVarInsn(ALOAD, 4);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "c", "()I");
-mv.visitJumpInsn(IF_ICMPNE, l4);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "d", "()J");
-mv.visitVarInsn(ALOAD, 4);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "d", "()J");
-mv.visitInsn(LCMP);
-mv.visitJumpInsn(IFNE, l4);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "e", "()F");
-mv.visitVarInsn(ALOAD, 4);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "e", "()F");
-mv.visitInsn(FCMPL);
-mv.visitJumpInsn(IFNE, l4);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "f", "()D");
-mv.visitVarInsn(ALOAD, 4);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "f", "()D");
-mv.visitInsn(DCMPL);
-mv.visitJumpInsn(IFNE, l4);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "g", "()C");
-mv.visitVarInsn(ALOAD, 4);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "g", "()C");
-mv.visitJumpInsn(IF_ICMPNE, l4);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "h", "()Ljava/lang/String;");
-mv.visitVarInsn(ALOAD, 4);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "h", "()Ljava/lang/String;");
-mv.visitVarInsn(ASTORE, 5);
-mv.visitInsn(DUP);
-val l5 = new Label();
-mv.visitJumpInsn(IFNONNULL, l5);
-mv.visitInsn(POP);
-mv.visitVarInsn(ALOAD, 5);
-val l6 = new Label();
-mv.visitJumpInsn(IFNULL, l6);
-mv.visitJumpInsn(GOTO, l4);
-mv.visitLabel(l5);
-mv.visitFrame(Opcodes.F_FULL, 6, Array[Object] ("models/MyRecord", "java/lang/Object", "java/lang/Object", Opcodes.INTEGER, "models/MyRecord", "java/lang/String"), 1, Array[Object] ("java/lang/String"));
-mv.visitVarInsn(ALOAD, 5);
-mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Object", "equals", "(Ljava/lang/Object;)Z");
-mv.visitJumpInsn(IFEQ, l4);
-mv.visitLabel(l6);
-mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "i", "()Z");
-mv.visitVarInsn(ALOAD, 4);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "i", "()Z");
-mv.visitJumpInsn(IF_ICMPNE, l4);
-mv.visitVarInsn(ALOAD, 4);
-mv.visitVarInsn(ALOAD, 0);
-mv.visitMethodInsn(INVOKEVIRTUAL, "models/MyRecord", "canEqual", "(Ljava/lang/Object;)Z");
-mv.visitJumpInsn(IFEQ, l4);
-mv.visitInsn(ICONST_1);
-val l7 = new Label();
-mv.visitJumpInsn(GOTO, l7);
-mv.visitLabel(l4);
-mv.visitFrame(Opcodes.F_CHOP,1, null, 0, null);
-mv.visitInsn(ICONST_0);
-mv.visitLabel(l7);
-mv.visitFrame(Opcodes.F_SAME1, 0, null, 1, Array[Object] (Opcodes.INTEGER));
-mv.visitJumpInsn(IFEQ, l3);
-mv.visitLabel(l0);
-mv.visitFrame(Opcodes.F_CHOP,3, null, 0, null);
-mv.visitInsn(ICONST_1);
-val l8 = new Label();
-mv.visitJumpInsn(GOTO, l8);
-mv.visitLabel(l3);
-mv.visitFrame(Opcodes.F_APPEND,2, Array[Object] ("java/lang/Object", Opcodes.INTEGER), 0, null);
-mv.visitInsn(ICONST_0);
-mv.visitLabel(l8);
-mv.visitFrame(Opcodes.F_FULL, 2, Array[Object] ("models/MyRecord", "java/lang/Object"), 1, Array[Object] (Opcodes.INTEGER));
-mv.visitInsn(IRETURN);
-mv.visitMaxs(4, 6);
-mv.visitEnd();
-}
+    mv = cw.visitMethod(ACC_PUBLIC, "equals", "(Ljava/lang/Object;)Z", null, null);
+    mv.visitCode();//if there's a "Nothing" then drop all value members after the first "Nothing".
+    val fields = (if (fieldData.map(n=>n.fieldType).contains("Nothing")) fieldData.reverse.dropWhile(valueMember =>   valueMember.fieldType != "Nothing").reverse; else fieldData)
+    var l0: Label = null
 
-*/
+    if (fieldData.length > 0) {
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitVarInsn(ALOAD, 1);
+      l0 = new Label();
 
-
-mv = cw.visitMethod(ACC_PUBLIC, "equals", "(Ljava/lang/Object;)Z", null, null);
-mv.visitCode();//if there's a "Nothing" then drop all value members after the first "Nothing".
-val fields = (if (fieldData.map(n=>n.fieldType).contains("Nothing")) fieldData.reverse.dropWhile(valueMember =>  valueMember.fieldType != "Nothing").reverse; else fieldData)
-var l0: Label = null
-
-if (fieldData.length > 0) {
-  mv.visitVarInsn(ALOAD, 0);
-  mv.visitVarInsn(ALOAD, 1);
-  l0 = new Label();
-
-  if (fieldData.map(n => n.fieldType).forall(t => List("Nothing").contains(t))) {//if all the valueMembers are "Nothing"
-    mv.visitJumpInsn(IF_ACMPNE, l0);
-    mv.visitInsn(ICONST_1);
-    val nothingLabel = new Label();
-    mv.visitJumpInsn(GOTO, nothingLabel);
-    mv.visitLabel(l0);
-    mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
-  }
-  else {
-    mv.visitJumpInsn(IF_ACMPEQ, l0);
-  }
-}
-
-
+      if (fieldData.map(n => n.fieldType).forall(t => List("Nothing").contains(t))) {//if all the valueMembers are "Nothing"
+        mv.visitJumpInsn(IF_ACMPNE, l0);
+        mv.visitInsn(ICONST_1);
+        val nothingLabel = new Label();
+        mv.visitJumpInsn(GOTO, nothingLabel);
+        mv.visitLabel(l0);
+        mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+      } 
+      else {
+        mv.visitJumpInsn(IF_ACMPEQ, l0);
+      }
+    }
 
 mv.visitVarInsn(ALOAD, 1);
 mv.visitVarInsn(ASTORE, 2);
@@ -166,8 +36,6 @@ mv.visitVarInsn(ALOAD, 2);
 mv.visitTypeInsn(INSTANCEOF, caseClassName);
 val l1 = new Label();
 mv.visitJumpInsn(IFEQ, l1);
-
-
 
 mv.visitInsn(ICONST_1);
 mv.visitVarInsn(ISTORE, 3);
@@ -201,9 +69,6 @@ fields.foreach(valueMember => {
       if (fieldData.indexOf(valueMember) == 0) valueMembersGOTOLabel = new Label();
       mv.visitJumpInsn(IF_ICMPNE, valueMembersGOTOLabel);
 
-
-
-
     }
     case "Double"|"Float"|"Long" => {
       mv.visitVarInsn(ALOAD, 0);
@@ -221,10 +86,6 @@ fields.foreach(valueMember => {
         mv.visitFieldInsn(GETSTATIC, "scala/runtime/BoxedUnit", "UNIT", "Lscala/runtime/BoxedUnit;");
         mv.visitFieldInsn(GETSTATIC, "scala/runtime/BoxedUnit", "UNIT", "Lscala/runtime/BoxedUnit;");
       }
-
-
-
-
 
       else {
         mv.visitVarInsn(ALOAD, 0);
